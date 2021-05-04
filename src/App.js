8dd4fb71caf370aea22814e9e4c4a8174bb1a0a0
nhuanthi <tlam1288@gmail.com>
2021-05-04 14:41:38 -0700

    called api with fetch and rednered movie search to page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
 import Movie from "./components/Movie";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "The Avengers",
-      Year: "2012",
-      imdbID: "tt0848228",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-    },
-    {
-      Title: "Avengers: Infinity War",
-      Year: "2018",
-      imdbID: "tt4154756",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Avengers: Endgame",
-      Year: "2019",
-      imdbID: "tt4154796",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  function searchMovie() {
+    const api = "http://www.omdbapi.com/?apikey=8de7c732&s=titanic";
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.Search) {
+          setMovies(data.Search);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    searchMovie();
+  }, []);
 
   return (
-    <div className="app">
-      <div className="row">
+    <div>
+      <div>
         <Movie movies={movies} />
       </div>
     </div>
